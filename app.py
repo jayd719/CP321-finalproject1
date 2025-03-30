dc6ac0f7776ff97a166c92d288156acbdcca9322
jd <jashansingh29@gmail.com>
2025-03-29 23:13:42 -0400

    asdf

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ from src.setup import *
 import os
 
 
+BASE_PATH = os.getenv("DASH_BASE_PATHNAME", "/")
+
 app = dash.Dash(
     __name__,
     external_scripts=JS_SCRIPTS,
     external_stylesheets=SYTLE_SHEETS,
+    url_base_pathname=BASE_PATH,
 )
 
 # Initialize MVC components
